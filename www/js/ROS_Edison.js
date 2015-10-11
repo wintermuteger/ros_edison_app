ca11334b3366d1d69c4044b75cde50df79961390
Christoph <Christoph@mail.de>
2015-10-11 20:52:03 +0200

    Added ROS_Edison object to hold all configuration data and methods.

---
+++ www/js/ROS_Edison.js
@@ -0,0 +1,27 @@
+//-------------------------------------
+//Constructor for ROS_Edison object
+//-------------------------------------
+function ROS_Edison()
+{
+    this.ip_string = "127.0.0.1";
+    this.port_string = "9090";
+    this.ros = null;
+}
+
+//-------------------------------------
+//Connection function
+//-------------------------------------
+ROS_Edison.prototype.connect = function()
+{
+    if(this.ros != null)
+    {
+        //Shutdown connection
+        //TBD
+        this.ros = null;
+    }
+    
+    var ws_string = "ws://" + this.ip_string + ":" + this.port_string;
+   // this.ros = new ROSLIB.Ros({url: ws_string});
+    
+    $("#app-status").html("<b>Status:</b> Connected to " + this.ip_string + ":" + this.port_string);
+}
