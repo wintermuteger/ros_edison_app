ffb1423ff12c02da4e69cca991026130107fe778
Christoph <Christoph@mail.de>
2016-09-16 22:02:42 +0200

    Intermediate checkin

---
+++ www/js/ROS_Touchpad.js
@@ -0,0 +1,12 @@
+//-------------------------------------
+//Constructor for ROS_Touchpad Object
+//-------------------------------------
+function ROS_Touchpad(touch_obj)
+{
+    this.$touchdiv = touch_obj;
+}
+
+ROS_Touchpad.prototype.dummy = function()
+{
+    
+}
