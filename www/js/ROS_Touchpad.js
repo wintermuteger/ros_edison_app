fd8ba22f5f3386acd45ef6514ca3b432e6a2cbc9
Christoph <Christoph@mail.de>
2016-09-16 22:03:22 +0200

    redo intermediate checkin

--- www/js/ROS_Touchpad.js
+++ www/js/ROS_Touchpad.js
@@ -4,9 +4,40 @@
 function ROS_Touchpad(touch_obj)
 {
     this.$touchdiv = touch_obj;
+    this.x = 0;
+    this.y = 0;
+    
+    //Create a new div inside the $touchdiv
+    this.$touchdiv.html("<div id=\"touchcursor\">Cur</div>");
+    //Store the touchcursor
+    this.$cursor = $("#touchcursor");
+    this.$cursor.width(Math.round(this.getWidth()/10));
+    this.$cursor.height(Math.round(this.getHeight()/10));
+    this.$cursor.css("background-color", "red");
+    
+    this.$touchdiv.css("background-color","green");
 }
 
-ROS_Touchpad.prototype.dummy = function()
+ROS_Touchpad.prototype.getWidth = function()
 {
+    if(this.$touchdiv !== null)
+    {
+        return this.$touchdiv.width();        
+    }
+    else
+    {
+        return 1;
+    }
+}
     
+ROS_Touchpad.prototype.getHeight = function()
+{
+    if(this.$touchdiv !== null)
+    {
+        return this.$touchdiv.height();        
+    }
+    else
+    {
+        return 1;
+    }
 }
