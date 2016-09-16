24e501bf06775e6dbcb6d81f58c4cf03123c6658
Christoph <schultzc83@gmail.com>
2016-09-17 00:19:48 +0200

    Fix of first setup of elements that will become the touchpad control element.

--- www/js/ROS_Touchpad.js
+++ www/js/ROS_Touchpad.js
@@ -1,21 +1,42 @@
 //-------------------------------------
 //Constructor for ROS_Touchpad Object
 //-------------------------------------
-function ROS_Touchpad(touch_obj)
+function ROS_Touchpad()
 {
-    this.$touchdiv = touch_obj;
+    this.$touchdiv = null;
     this.x = 0;
     this.y = 0;
-    
+    this.$cursor = null;
+   
+}
+
+ROS_Touchpad.prototype.setup = function(touch_obj)
+{
+    this.$touchdiv = touch_obj;
     //Create a new div inside the $touchdiv
-    this.$touchdiv.html("<div id=\"touchcursor\">Cur</div>");
+    this.$touchdiv.html("<div id=\"touchcursor\"></div>");
     //Store the touchcursor
     this.$cursor = $("#touchcursor");
     this.$cursor.width(Math.round(this.getWidth()/10));
     this.$cursor.height(Math.round(this.getHeight()/10));
     this.$cursor.css("background-color", "red");
+    this.$cursor.css("position", "relative");
     
     this.$touchdiv.css("background-color","green");
+    this.update();
+}
+
+ROS_Touchpad.prototype.update = function()
+{
+    
+    var scaley = Math.round(this.getWidth()/2);
+    var scalex = Math.round(this.getHeight()/2);
+    var offy = Math.round(this.getHeight()/2)-Math.round(this.$cursor.height()/2);
+    
+    this.$cursor.css("left", (this.x*scalex) + "px");
+    this.$cursor.css("top", (this.y*scaley+offy) + "px");
+    
+    this.$cursor.html(this.x + "/" + this.y);
 }
 
 ROS_Touchpad.prototype.getWidth = function()
